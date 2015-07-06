37c68365f753c86e61588fa4d2656edae52db0af
Gavin Young <gavinyoung1994@gmail.com>
2015-07-06 11:55:08 -0400

    It is live now

--- public/javascript/jquery.js
+++ public/javascript/jquery.js
@@ -19,6 +19,27 @@ function draw (src, list, check){
 	}
 }
 
+function initialize () {
+		mapOptions = {
+		        center: myLatlng,
+		        zoom: 13,
+		        mapTypeId: google.maps.MapTypeId.ROADMAP,
+		        styles: styleArr
+		    };
+		map = new google.maps.Map(map_canvas_obj, mapOptions);
+}
+
+function bound () {
+	if (!arrayOfDays[current-1].markers.length) {initialize()}
+	else {
+		var bound = new google.maps.LatLngBounds();
+		for (var i = 0; i < arrayOfDays[current - 1].markers.length; i++) {
+			bound.extend(arrayOfDays[current-1].markers[i].position);
+		}
+		map.fitBounds(bound);
+	}
+}
+
 function addIn (button,list, element,day) {
 	$(button).click(function(){
 		var dom = '<div class="itinerary-item"><span class="title">' + $('#' + element)[0].value + '</span><button class="btn btn-xs btn-danger remove btn-circle">x</button></div>';
@@ -33,7 +54,7 @@ function addIn (button,list, element,day) {
 			draw('/images/star-3.png',all_things_to_do,$('#' + element)[0].value);
 			arrayOfDays[current-1].things.push(dom);
 		}
-		// console.log(arrayOfDays);
+		bound();
 	})
 }
 
@@ -57,10 +78,11 @@ function deleteWhenDelete() {
 		       arrayOfDays[current-1].rest.splice(i, 1);
 		}
 		for (var i = 0; i < arrayOfDays[current-1].things.length; i++) {
-		   console.log(arrayOfDays[current-1].things)
+		   // console.log(arrayOfDays[current-1].things)
 		   if (arrayOfDays[current-1].things[i] === $(this).closest('div')[0].outerHTML)
 		       arrayOfDays[current-1].things.splice(i, 1);
 		}
+		bound();
 	});
 }
 
@@ -70,18 +92,9 @@ $('#add-day').click(function () {
 	arrayOfDays.push({hotel:[],rest:[],things:[],markers:[]});
 })
 
-// changing days
-$('.day-buttons').delegate('.day-btn', 'click', function () {
-    $('.current-day').removeClass('current-day');
-    $(this).addClass('current-day');
-    // remove items from the day
-    $('.itinerary-item').remove();
-    arrayOfDays[current-1].markers.forEach(function(marker) {marker.setMap(null);})
-    current = parseInt($(this).text());
-    // changing the day title
-
-    console.log($('#day-title').children(0)[0].innerHTML)
-    $('#day-title').children(0)[0].innerHTML = 'Day ' + current;
+function resetHTML(){
+	$('.itinerary-item').remove();
+	$('#day-title').children(0)[0].innerHTML = 'Day ' + current;
     for(var i=0; i<arrayOfDays[current-1].markers.length;i++){
         arrayOfDays[current-1].markers[i].setMap(map);
     }
@@ -94,13 +107,58 @@ $('.day-buttons').delegate('.day-btn', 'click', function () {
     for (var i = 0; i < arrayOfDays[current-1].things.length; i++) {
         $("#t-list").append(arrayOfDays[current-1].things[i]);
     };
+}
+
+
+function changeOrderDays () {
+	for (var i = current-1; i < $('#day').children().length - 1; i++) {
+		$('#day').children()[i].innerHTML = parseInt($('#day').children()[i].innerHTML) - 1;
+	}
+
+}
+
+// changing days
+$('.day-buttons').delegate('.day-btn', 'click', function () {
+	$('.current-day').removeClass('current-day');
+    $(this).addClass('current-day');
+    // remove items from the day
+    arrayOfDays[current-1].markers.forEach(function(marker) {marker.setMap(null);})
+    current = parseInt($(this).text());
+    // changing the day title
+
+    resetHTML();
+    bound();
 })
 
+
+$('#delete-day').click(function(){
+	if(arrayOfDays.length>1 && current != 1){
+		arrayOfDays[current-1].markers.forEach(function(marker) {marker.setMap(null);})
+		arrayOfDays.splice(current-1,1);
+		$('.current-day').remove();
+		changeOrderDays();
+		current = 1;
+		$("#first").addClass("current-day");
+		resetHTML();
+		bound();
+	}
+	else if (arrayOfDays.length>1 && current === 1) {
+		arrayOfDays[current-1].markers.forEach(function(marker) {marker.setMap(null);})
+		arrayOfDays.splice(current-1,1);
+		$('.current-day').remove();
+		changeOrderDays();
+		current = arrayOfDays.length;
+		$("#add-day").prev().addClass("current-day");
+		resetHTML();
+		bound();
+	}
+});
 
 addIn('#h-btn', '#h-list', 'hotel');
 addIn('#r-btn', '#r-list', 'restaurant');
 addIn('#t-btn', '#t-list', 'thing');
 deleteWhenDelete();
+
 
 
 
